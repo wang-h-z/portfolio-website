88040ea5f9092d334522b690e9b2381f0fee44eb
wang-h-z <haozhenwang297@gmail.com>
2026-02-02 20:32:27 +0800

    made changes

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -2,13 +2,12 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Github, Linkedin, Mail, Twitter } from 'lucide-react';
+import { Github, Linkedin, Mail } from 'lucide-react';
 
 const socials = [
-  { name: 'GitHub', icon: Github, href: 'https://github.com' },
-  { name: 'LinkedIn', icon: Linkedin, href: 'https://linkedin.com' },
-  { name: 'Twitter', icon: Twitter, href: 'https://twitter.com' },
-  { name: 'Email', icon: Mail, href: 'mailto:hello@example.com' },
+  { name: 'GitHub', icon: Github, href: 'https://github.com/wang-h-z' },
+  { name: 'LinkedIn', icon: Linkedin, href: 'https://www.linkedin.com/in/wanghaozhen//' },
+  { name: 'Email', icon: Mail, href: 'mailto:haozhenwang297@gmail.com' },
 ];
 
 export default function Footer() {
